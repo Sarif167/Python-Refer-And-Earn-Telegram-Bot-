f16b645ab31c248c04d5b795b93f8ce81e1c1612
Botmaster55 <wajsarif461@gmail.com>
2025-11-21 01:39:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ TOKEN = "TRON"
 
 BOT_TOKEN = "8423595453:AAHPgdKl7-9F2U2tA9IPW70wktFxD75e7QQ"
 PAYMENT_CHANNEL = "@ClipMateBhai" #add payment channel here including the '@' sign
-OWNER_ID = 1350180828 #write owner's user id here.. get it from @MissRose_Bot by /id
+OWNER_ID = 1249672673 #write owner's user id here.. get it from @MissRose_Bot by /id
 CHANNELS = ["@ClipMateBhai"] #add channels to be checked here in the format - ["Channel 1", "Channel 2"] 
               #you can add as many channels here and also add the '@' sign before channel username
 Daily_bonus = 0.001 #Put daily bonus amount here!
